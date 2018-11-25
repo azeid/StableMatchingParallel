c2ccb8bc20f53eb682936e624e61240b8fab3bf8
Guillermo León <gleonoliva@users.noreply.github.com>
2018-11-24 23:13:02 -0600

    reusing threads forcing them to wait for more work

--- src/test/java/bench/BenchmarkRunner.java
+++ src/test/java/bench/BenchmarkRunner.java
@@ -4,15 +4,17 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 
 public class BenchmarkRunner {
 
     public static void main(String[] args) throws RunnerException {
 
         Options options = new OptionsBuilder()
-                .include(BenchmarkRandom10.class.getSimpleName())
-                .include(BenchmarkRandom100.class.getSimpleName())
+                //.include(BenchmarkRandom10.class.getSimpleName())
+                //.include(BenchmarkRandom100.class.getSimpleName())
                 .include(BenchmarkRandom200.class.getSimpleName())
+                .timeout(TimeValue.minutes(1))
                 .warmupIterations(3)
                 .forks(1)
                 .build();
