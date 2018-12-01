21f20144de38d2d1e93727092b0db112fc9fab88
azeid <44010260+azeid@users.noreply.github.com>
2018-11-30 22:59:50 -0600

    include package to file
    this is causing an error

--- src/main/java/smp/JavaSMPMasterSlave.java
+++ src/main/java/smp/JavaSMPMasterSlave.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package smp;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
